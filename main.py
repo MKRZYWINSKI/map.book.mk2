06269a87e7dbc5a17bb767d78f13db751a8c10c6
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-29 16:00:18 +0200

    wip: add a menu

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ def read_users(user_list: list[dict]) -> None:
         print(f"Twój znajomy {user['name']}: {user['surname']}, opublikował{user['posts']} postów")
 
         read_users(users)
+
+
+
+if __name__ == "__main__":
+        print("Witaj użytkowniku")
+        while True:
+            print("Menu:")
+            print("0.Zakoncz program")
+            print("1.Wyswietl co u znajomych")
+            manu_option: str = input("Dokonaj wyboru")
+            if manu_option == "0":
+                print("Program kończy prace")
+                break
+            if manu_option == "1":
+                read_users(users)
