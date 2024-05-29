ae82776206b891a2603c5aa6297368786261ab13
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-29 15:43:09 +0200

    Add: users printing funciotionality

---
+++ main.py
@@ -0,0 +1,18 @@
+imie="Jakub"
+nazwisko="Łojek"
+liczba_postów="9999"
+
+print(f"Twój znjamowy {imie} {nazwisko} opublikował {liczba_postów} postów")
+
+
+imie2="Jakub"
+nazwisko2="Maicoh"
+liczba_postów2="9899"
+
+print(f"Twój znjamowy {imie2} {nazwisko2} opublikował {liczba_postów2} postów")
+
+imie3="Jakub"
+nazwisko3="Kołodziejczyk"
+liczba_postów3="99"
+
+print(f"Twój znjamowy {imie3} {nazwisko3} opublikował {liczba_postów3} postów")
