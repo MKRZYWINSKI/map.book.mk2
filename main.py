ed7d26bf931f1e22336bf7f9dcc75f7591d16c69
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-29 17:09:44 +0200

    wip: add a mapp location funciton

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from models.data import users
-from utils.crud import read_users,add_user,search_user,remove_user
+from utils.crud import read, add_user,search,remove_user,update_user,map_single_users,map_all_users
 
+if __name__ == '__main__':
+    print(f'witaj {users[0]["name"]}')
 
-if __name__ == "__main__":
-     print("Witaj użytkowniku")
-     while True:
-         print("Menu:")
-         print("0.Zakoncz program")
-         print("1.Wyswietl co u znajomych")
-         print("2.Dodaj znajomego")
-         print("3.Szukaj użytkownika")
-         print("4.Usuń użytkownika")
-         manu_option: str = input("Dokonaj wyboru")
-         if  manu_option == "0":
-             print("Program kończy prace")
-             break
-         if  manu_option == "1":
-                read_users(users)
-         if manu_option == "2":
-             add_user(users)
-         if manu_option == "3":
-             search_user(users)
-         if manu_option == "4":
-             remove_user(users)
+    while True:
+        print('0. zakończ program ')
+        print('1. wyświetl znajomych ')
+        print('2. dodaj znajomego ')
+        print('3. szukaj znajomego ')
+        print('4. usuń znajomego ')
+        print('5. kogo zaktualizować')
+        print('6. wyświetl mapę dla każdego użytkownika')
+        print('7. wyświetl zbiorczą mapę')
+        menu_option = input('wybierz opcje menu: ')
+        if menu_option == '0': break
+        if menu_option == '1': read(users)
+        if menu_option == '2': add_user(users)
+        if menu_option == '3': search(users)
+        if menu_option == '4': remove_user(users)
+        if menu_option == '5': update_user(users)
+        if menu_option == '6':
+            for user in users:
+                map_single_users(user['name'],user['post'],user['location'])
+        if menu_option == '7':map_all_users(users)
--- models/data.py
+++ models/data.py
@@ -1,5 +1,8 @@
-users: list[dict] = [
-    {"name":"Kacper", "surname":"Macioch", "posts": 999 },
-    {"name":"Michał", "surname":"Lębryk", "posts": 979 },
-    {"name":"Dominik", "surname":"Kużnik", "posts": 989 },
-    {"name":"Leon", "surname":"Hajdus", "posts": 229 }]
+users: list[dict[str, str]] = [
+    {'name': 'Staś', 'surname': 'Grzymski', 'post': 1, 'location':'Konin'},
+    {'name': 'Kacper', 'surname': 'Macioch', 'post': 2, 'location':'Ząbki'},
+    {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3, 'location':'Brodnica'},
+    {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2, 'location':'Toruń'},
+    {'name': 'Michał', 'surname': 'Lębryk', 'post': 2, 'location':'Lublin'},
+]
+
