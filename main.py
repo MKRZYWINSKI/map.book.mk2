b0a2a1d63f5df32e1d363642471f5d1b27688aa9
MKRZYWINSKI <u56106@student.wat.edu.pl>
2024-05-29 16:48:32 +0200

    wip: add remove user function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data import users
-from utils.crud import read_users,add_user
+from utils.crud import read_users,add_user,search_user,remove_user
 
 
 if __name__ == "__main__":
@@ -9,6 +9,8 @@ if __name__ == "__main__":
          print("0.Zakoncz program")
          print("1.Wyswietl co u znajomych")
          print("2.Dodaj znajomego")
+         print("3.Szukaj użytkownika")
+         print("4.Usuń użytkownika")
          manu_option: str = input("Dokonaj wyboru")
          if  manu_option == "0":
              print("Program kończy prace")
@@ -17,3 +19,7 @@ if __name__ == "__main__":
                 read_users(users)
          if manu_option == "2":
              add_user(users)
+         if manu_option == "3":
+             search_user(users)
+         if manu_option == "4":
+             remove_user(users)
